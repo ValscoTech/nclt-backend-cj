88fff64fe55e5c47854a518f2962ec7db75a6a90
srikrishna <srikrishnaedu18@gmail.com>
2026-03-06 11:22:04 +0530

    Delete redundant filling number function in  party details

--- src/routes/party.routes.js
+++ src/routes/party.routes.js
@@ -1,13 +1,9 @@
 import express from "express";
-import {
-  fetchByPartyName,
-  fetchPartyDetails,
-} from "../controllers/party.controller.js";
+import { fetchByPartyName } from "../controllers/party.controller.js";
 
 const router = express.Router();
 
 router.post("/party-name", fetchByPartyName);
 router.post("/party", fetchByPartyName);
-router.get("/party/:filingNo", fetchPartyDetails);
 
 export default router;
